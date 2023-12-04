93d09809bb0765898bbbc364e6673d024dcff986
justbeingkat <k.y.vanveen@tilburguniversity.edu>
2023-12-04 17:11:47 +0100

    finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,15 @@
-input = open("input.txt", "r")
-lines = input.readlines()
+inputText = open("input.txt", "r")
+lines = inputText.readlines()
 
+numberList = []
+
+for line in lines:
+    num = ""
+    for char in line:
+        if ":" > char > "/":
+            num += char
+    num = num[0] + num[-1]
+    numberList.append(int(num))
+print(numberList)
+totalSum = sum(numberList)
+print(totalSum)
