6e69b4875495407d5031bcb41eb5b9b45d752428
justbeingkat <k.y.vanveen@tilburguniversity.edu>
2023-12-07 12:18:56 +0100

    finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,24 @@
 inputText = open("input.txt", "r")
 lines = inputText.readlines()
 
+numberDict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
 numberList = []
 
 for line in lines:
     num = ""
+    newstring = ""
     for char in line:
-        if ":" > char > "/":
+        if "/" < char < ":":
             num += char
+        else:
+            newstring += char
+            for number in numberDict:
+                if number in newstring:
+                    num += str(numberDict[number])
+                    newstring = newstring[-1]
+    print(num)
     num = num[0] + num[-1]
     numberList.append(int(num))
-print(numberList)
 totalSum = sum(numberList)
 print(totalSum)
